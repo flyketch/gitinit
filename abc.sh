93fb44ac6a7e1974526ee005e1ed5a7fd7cf27a4
Steve <shifeiling@sina.com>
2018-01-19 13:45:31 +0800

    add abc.sh

--- abc.sh
+++ abc.sh
@@ -8,3 +8,15 @@ a="Hello World"
 
 echo "a is $a"
 echo 'a is $a'
+
+var_a="Linux"
+readonly var_a
+var_a="Unix"
+echo "var_a is $var_a"
+
+b="BB34"
+echo ${b}
+d=${b/BB/12}
+echo $d
+let "d+=1"
+echo "d is $d"
