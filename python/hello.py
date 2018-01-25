a56795ba94b10eccae775e298d68cbc6e7c36141
Steve <shifeiling@sina.com>
2018-01-25 13:10:44 +0800

    update if case

--- python/hello.py
+++ python/hello.py
@@ -1,1 +1,12 @@
-print ('Hello World'); #注意到print是一个函数
+# coding=utf-8
+print ('Hello World') #注意到print是一个函数
+
+name = 'steve'
+age = '20'
+print('{0} is {1} years old'.format(name, age))
+
+print('another {} is {} years old'.format(name, age))
+print(str(name) + ' is ' + str(age) + ' years old')
+
+print({3**3});
+
