e421905642df8b90fec621a3b62d917aa61958c1
steve <steve@vonbross-iMac.local>
2018-03-21 10:30:22 +0800

    update

--- swift/MyPlayground.playground/Contents.swift
+++ swift/MyPlayground.playground/Contents.swift
@@ -245,11 +245,32 @@ for i in 1...power {
 print(answer)
 print(result)
 
+var should: Bool {
+    return true
+}
 
+should
 
+let finalSquare = 25
+var board = [Int](repeating: 0, count: finalSquare + 1)
 
+board[03] = +08; board[06] = +11; board[09] = +09; board[10] = +02
+board[14] = -10; board[19] = -11; board[22] = -02; board[24] = -08
 
-
+var square = 0
+var diceRoll = 0
+while square < finalSquare {
+    // 掷骰子
+    diceRoll += 1
+    if diceRoll == 7 { diceRoll = 1 }
+    // 根据点数移动
+    square += diceRoll
+    if square < board.count {
+        // 如果玩家还在棋盘上，顺着梯子爬上去或者顺着蛇滑下去
+        square += board[square]
+    }
+}
+print("Game over!")
 
 
 
