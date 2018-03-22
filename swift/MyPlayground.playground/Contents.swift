131cbe0eff5e1073621a40aa976a006e01770aa9
steve <steve@vonbross-iMac.local>
2018-03-22 13:49:56 +0800

    update file

--- swift/MyPlayground.playground/Contents.swift
+++ swift/MyPlayground.playground/Contents.swift
@@ -273,6 +273,30 @@ while square < finalSquare {
 print("Game over!")
 
 
+enum compress {
+    case east
+    case south
+    case west
+    case north
+}
+
+var directionToHead = compress.east
+
+directionToHead = compress.south
+
+switch directionToHead {
+case .north:
+    print("Lots of planets have a north")
+case .south:
+    print("Watch out for penguins")
+case .east:
+    print("Where the sun rises")
+case .west:
+    print("Where the skies are blue")
+}
+
+
+
 
 
 
