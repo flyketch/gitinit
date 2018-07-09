44b47c42a8d0ff6a17cc266c4452d8afc92948a0
flyketch <809852506@qq.com>
2018-07-09 16:47:26 +0800

    add java

---
+++ java/Test.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Test {
+    public static void main(String[] args) {
+        String tt = "Hello";
+        if (tt == "Hellos") {
+            System.out.println("true in if");
+        }else {
+            System.out.println("false in if");
+        }
+
+        System.out.println("out side if");
+    }
+}
